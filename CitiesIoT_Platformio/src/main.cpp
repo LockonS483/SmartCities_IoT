456fba8a86b0550de12889297c8795c7acb83812
LockonS483 <rumblie.gg@gmail.com>
2018-11-09 12:32:24 -0500

    Sound Sensor test in platform

--- CitiesIoT_Platformio/src/main.cpp
+++ CitiesIoT_Platformio/src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
 
+//Variables
+const int envelopePin = A0;
+int sensorValue = 0;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  sensorValue = analogRead(envelopePin);
+  Serial.print("Envelope Reading: "); Serial.println(sensorValue);
+  delay(250);
 }
